0937104ffacdf651b304001c76b6bcd5f31c530f
Gary Wisniewski <garyw@blueseastech.com.au>
2015-05-19 12:36:50 +1000

    Got syslog filter format working properly.

--- cutil/syslog.py
+++ cutil/syslog.py
@@ -20,7 +20,7 @@ _PRIORITY_DICT = {_PRIORITY[i]:i for i in range(len(_PRIORITY))}
 _PRIORITY_DICT['warning'] = _PRIORITY_DICT['warn']
 _PRIORITY_DICT['error'] = _PRIORITY_DICT['err']
 
-_RE_SPEC = re.compile(r'^(?P<fpfx>!?)(?:/(?P<regex>.+)/|\[(?P<prog>.+)\]|(?P<fac>[*a-zA-Z]+))\.(?P<pfx>!?=?)(?P<pri>[*a-zA-Z]+)$')
+_RE_SPEC = re.compile(r'^(?P<fpfx>!?)(?:/(?P<regex>.+)/|\[(?P<prog>.+)\]|(?P<fac>[,*a-zA-Z]+))\.(?P<pfx>!?=?)(?P<pri>[*a-zA-Z]+)$')
 _RE_SPECSEP = re.compile(r' *; *')
 
 _RE_SYSLOG = re.compile(r'^<(?P<pri>\d+)>(?P<date>\w{3} [ 0-9][0-9] \d\d:\d\d:\d\d) (?P<prog>[^ \[]+)(?P<rest>\[\d+\]: .+)$')
@@ -42,20 +42,66 @@ class _syslog_spec_matcher:
        where prog will match the program specifier, if any
 
     One or more of the above can be combined, separated by semicolons.
+
+    Note that the syslogd semantics are hard to actually figure out, even if you scour the web.  So, here are
+    some rules.
+
+    The semicolon "joins" constraints by combining all negative constraints (those which omit facilities or priorities)
+    and positive constraints separately.   The result will be logged ONLY if all the positive constraints are true
+    and all of the negative constraints are false!
+
+    So,
+       *.!emerg               LOGS NOTHING (missing inclusions)
+       *.*;*.!emerg           logs everything bug .emerg
+       *.info;![cron].*       logs all info or higher, but omits everything from program "cron"
+       *.*;![cron].!=info     Omits the info messages from any program BUT cron
+       [cron].*;*.!info       includes all cron messages except those of info and above
+
+    More specifically:
+       *.info                 Includes info through emergency (6->0) but not Debug
+       *.!info                Excludes info through emergency but does not exclude debug
+       *.=info                Includes just info itself
+       *.!=info               Excludes everything BUT info
+       !f.!=info              Excludes everyting BUT info from everything BUT f
+
+    Why all this bother?
+    1.  Basic cases are pretty easy to read and understand.
+    2.  Negations can be understood if documented, and are useful.
+    3.  I don't want to introduce a completely new syntax.
+    3.  Somewhere out here, there is some nerdy OCD guy who will say "But wait, your filter format is so CLOSE
+        to the syslog format that you MUST support it with the same semantics or you're going to alienate [me]."  
+        Just nipping that in the bud.
     """
 
-    __slots__ = ('_regexes', '_matchlist', 'debuglist')
+    __slots__ = ('_regexes', '_match', 'debugexpr')
 
     def __init__(self, speclist):
         self._regexes = []
-        self._matchlist = []
-        self.debuglist = []
 
         pieces = _RE_SPECSEP.split(speclist)
-        for p in pieces:
-            self._init_spec(p)
 
-    def _init_spec(self, spec):
+        # Build the list of negations and positive expressions
+        neg = list()
+        pos = list()
+        for p in pieces:
+            self._init_spec(p, neg, pos)
+
+        if not pos:
+            self._buildex("False")
+        elif not neg:
+            self._buildex(" or ".join(pos))
+        else:
+            self._buildex("(" + (" or ".join(neg)) + ") and (" + (" or ".join(pos)) + ")")
+
+    def _buildex(self, expr):
+        # Perform some quick peepole optimization, then compile
+        nexpr = expr.replace("True and ", "").replace(" and True", "")
+        nexpr = nexpr.replace("not True", "False").replace("and ((True))", "")
+        nexpr = nexpr.replace("False or ", "")
+        self.debugexpr = nexpr
+        self._match = eval("lambda s,p,f,g,buf: " + nexpr)
+
+    def _init_spec(self, spec, neg, pos):
         match = _RE_SPEC.match(spec)
 
         if not match:
@@ -67,48 +113,48 @@ class _syslog_spec_matcher:
 
         if gdict['regex'] is not None:
             self._regexes.append(re.compile(gdict['regex'], re.IGNORECASE))
-            s = '(bool(buf.search(s._regexes[%d])))' % (len(self._regexes) - 1)
+            c1 = 'bool(buf.search(s._regexes[%d]))' % (len(self._regexes) - 1)
         elif gdict['prog'] is not None:
-            s = '(g and "%s" == g.lower())' % gdict['prog'].lower()
+            c1 = '(g and "%s" == g.lower())' % gdict['prog'].lower()
         elif gdict['fac'] != '*':
-            fac = _FACILITY_DICT.get(gdict.get('fac'))
-            if fac == None:
+            faclist = [_FACILITY_DICT.get(f) for f in gdict.get('fac', '').split(',')]
+            if None in faclist:
                 raise Exception("Invalid logging facility code, %s: %s" % (gdict['fac'], spec))
-            s = '(f==%d)' % fac
+            c1 = '(' + ' or '.join(['f==%d' % f for f in faclist]) + ')'
         else:
-            s = None
-
-        if gdict['fpfx']:
-            e = ['(False)' if s is None else 'not ' + s]
-        elif s is not None:
-            e = [s]
-        else:
-            e = []              # all inclusive, no expr required
+            c1 = 'True'
 
         pri = gdict['pri']
         pfx = gdict.get('pfx', '')
 
         if pri == '*':
-            if '!' in pfx:
-                e.append('(False)') # silly, but it's a valid syntax
+            c2 = 'True'
         else:
             prival = _PRIORITY_DICT.get(pri)
             if prival == None:
                 raise Exception("Invalid logging priority, %s: %s" % (pri, spec))
-            s = "not " if '!' in pfx else ""
-
             if '=' in pfx:
-                s += "(p==%d)" % prival
+                c2 = "p==%d" % prival
             else:
-                s += "(p<=%d)" % prival
-            e.append(s)
+                c2 = "p<=%d" % prival
 
-        self.debuglist.append(e) # keep these so we can examine the logic if we need to
+        fpfx = gdict.get('fpfx', '')
 
-        if e:
-            expr = "lambda s,p,f,g,buf: " + " and ".join(e)
-            self._matchlist.append(eval(expr))
+        # Assess negatives and positives.
+        # neg will contain "EXCLUDE IF" and pos will contain "INCLUDE IF"
 
+        if '!' in fpfx:
+            # Double exclusion means to exclude everything except the given priority from
+            # everything except the given facility
+            if '!' in pfx:
+                neg.append("(not %s and not %s)" % (c1, c2))
+            else:
+                neg.append("not (%s and %s)" % (c1, c2))
+        elif '!' in pfx:
+            neg.append("(not %s or not %s)" % (c1, c2))
+        else:
+            pos.append("(%s and %s)" % (c1, c2))
+            
     def match(self, msg, prog = None, priority = SysLogHandler.LOG_ERR, facility = SysLogHandler.LOG_SYSLOG):
         for m in self._matchlist():
             if not m(self, priority, facility, prog, msg):
--- tests/syslog_spec.py
+++ tests/syslog_spec.py
@@ -3,25 +3,37 @@ from prefix import *
 from cutil.syslog import _syslog_spec_matcher
 
 SPECS = (
-    ('*.*',                    '[[]]'),
-    ('[crond].*',              "[['(g and \"crond\" == g.lower())']]"),
-    ('.*',                     "Invalid log spec syntax: .*"),
-    ('*.=emerg;*.=crit',       "[['(p==0)'], ['(p==2)']]"),
-    ('/not and\/or able/.*',   "[['(bool(buf.search(s._regexes[0])))']]"),
-    ('kern.*',                 "[['(f==0)']]"),
-    ('*.!*',                   "[['(False)']]"),
-    ('![chaperone].*',         "[['not (g and \"chaperone\" == g.lower())']]")
+    ('*.*',                                    '(True)'),
+    ('[crond].*',                              '((g and "crond" == g.lower()))'),
+    ('.*',                                     'Invalid log spec syntax: .*'),
+    ('kern.*;kern.!=crit',                     '((not (f==0) or not p==2)) and (((f==0)))'),
+    ('kern.*;kern.!crit',                      '((not (f==0) or not p<=2)) and (((f==0)))'),
+    ('kern.crit',                              '((f==0) and p<=2)'),
+    ('*.=emerg;*.=crit',                       '(p==0) or (p==2)'),
+    ('/not and\/or able/.*',                   '(bool(buf.search(s._regexes[0])))'),
+    ('kern.*',                                 '((f==0))'),
+    ('*.*;*.!*',                               '((False)) '),
+    ('*.*;![chaperone].*',                     '(not ((g and "chaperone" == g.lower()))) '),
+    ('kern.*;!auth,authpriv.*',                '(not ((f==4 or f==10))) and (((f==0)))'),
+    ('[cron].*;[daemon-tools].crit;/password/.!err', '((not bool(buf.search(s._regexes[0])) or not p<=3)) and (((g and "cron" == g.lower())) or ((g and "daemon-tools" == g.lower()) and p<=2))'),
+    ('kern.*;![cron].!err',                    '((not (g and "cron" == g.lower()) and not p<=3)) and (((f==0)))'),
+    ('[chaperone].err;[logrotate].err;!kern.*', '(not ((f==0))) and (((g and "chaperone" == g.lower()) and p<=3) or ((g and "logrotate" == g.lower()) and p<=3))'),
+    ('/panic/.*;/segfault/.*;*.!=debug',       '((not p==7)) and ((bool(buf.search(s._regexes[0]))) or (bool(buf.search(s._regexes[1]))))'),
 )
+
 
 class TestSyslogSpec(unittest.TestCase):
 
     def test_specs(self):
         for s in SPECS:
             try:
-                sm = _syslog_spec_matcher(s[0]).debuglist
+                sm = _syslog_spec_matcher(s[0]).debugexpr
             except Exception as ex:
                 sm = ex
-            #print(str(sm))
+                if 'unexpected' in str(sm):
+                    raise
+            #Uncomment to generate the test table, but CHECK IT carefully!
+            #print("('{0:40} '{1}'),".format(s[0]+"',", sm))
             self.assertEqual(str(sm), s[1])
 
 if __name__ == '__main__':
