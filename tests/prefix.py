97f93abadfcca8e0122d6dcf168a4d33a8f1a4f4
Gary Wisniewski <garyw@blueseastech.com.au>
2015-05-21 08:34:25 +1000

    Reorganized in anticipation of PyPi and setup issues.

--- tests/prefix.py
+++ tests/prefix.py
@@ -4,4 +4,5 @@ import unittest
 
 # Assure that packages in the same directory as ours (tests) can be used without concern for where
 # we are installed
-sys.path[0] = os.path.dirname(sys.path[0])
+sys.path[0] = os.path.join(os.path.dirname(sys.path[0]), "chaperone")
+
